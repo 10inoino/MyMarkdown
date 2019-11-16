2d03731e690433b1e65b2187858d26e69aaee585
Hikaru Inomata <inomatahi@icloud.com>
2019-11-16 20:10:54 +0900

    ログイン認証、ログアウトの実装

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,16 +1,25 @@
 <template>
     <div id="editor">
         <h1>エディター画面</h1>
+        <span>{{ user.displayName }}</span>
+        <button @click="logout">ログアウトしたい</button>
     </div>
 </template>
 
 <script>
 export default {
     name: "editor",
+    props: ['user'],
     data() {
-        return {};
+        return {}
+    },
+    methods: {
+        logout: function() {
+
+            firebase.auth().signOut();
+        }
     }
-};
+}
 </script>
 
 <style lang="scss">
