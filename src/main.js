25ffd42aa4e8597993f26ebb0d633e63bf35b189
Hikaru Inomata <inomatahi@icloud.com>
2019-11-17 20:30:50 +0900

    testcommit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import "./scss/style.scss";
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 new Vue({
   el: '#app',
+  router: router,
   render: h => h(App)
-})
+});
---
+++ src/views/Terms.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="terms">
+        <h1>利用規約</h1>
+        <p>まだ書けてないー</p>
+        <router-link :to="{ name: 'top' }">トップに戻る</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "terms"
+};
+</script>
